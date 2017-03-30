32000f18835fa0f8ec447800e7286f4ef5408890
simonprickett <simon@crudworks.org>
2017-03-29 21:42:53 -0700

    Adds comments.

--- basicdemo.py
+++ basicdemo.py
@@ -1,21 +1,28 @@
 import RPi.GPIO as GPIO
 import time
 
+# Setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(9, GPIO.OUT)
 GPIO.setup(10, GPIO.OUT)
 GPIO.setup(11, GPIO.OUT)
 
+# Loop forever
 while True:
+	# Red
 	GPIO.output(9, True)
 	time.sleep(3)
+	# Red and amber
 	GPIO.output(10, True)
 	time.sleep(1)
+	# Green
 	GPIO.output(9, False)
 	GPIO.output(10, False)
 	GPIO.output(11, True)
 	time.sleep(5)
+	# Amber
 	GPIO.output(11, False)
 	GPIO.output(10, True)
 	time.sleep(2)
+	# Amber off (red comes on at top of loop)
 	GPIO.output(10, False)
