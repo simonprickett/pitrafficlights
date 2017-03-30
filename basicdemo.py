eef63a1235dfc935cce77251f89ef01aa8fea80d
simonprickett <simon@crudworks.org>
2017-03-29 21:56:47 -0700

    UK v US pattern, tidy up.

--- basicdemo.py
+++ basicdemo.py
@@ -17,17 +17,21 @@ while True:
 	# Red
 	GPIO.output(9, True)
 	time.sleep(3)
+
 	# Red and amber
 	GPIO.output(10, True)
 	time.sleep(1)
+	
 	# Green
 	GPIO.output(9, False)
 	GPIO.output(10, False)
 	GPIO.output(11, True)
 	time.sleep(5)
+	
 	# Amber
 	GPIO.output(11, False)
 	GPIO.output(10, True)
 	time.sleep(2)
+	
 	# Amber off (red comes on at top of loop)
 	GPIO.output(10, False)
--- trafficlightpatterns.py
+++ trafficlightpatterns.py
@@ -13,7 +13,7 @@ GPIO.setup(9, GPIO.OUT)
 GPIO.setup(10, GPIO.OUT)
 GPIO.setup(11, GPIO.OUT)
 
-# Turn of all lights in case any are on from prior use
+# Turn off all lights in case any are on from prior use
 GPIO.output(9, False)
 GPIO.output(10, False)
 GPIO.output(11, False)
@@ -23,17 +23,26 @@ while True:
 	# Red
 	GPIO.output(9, True)
 	time.sleep(3)
-	# Red and amber
-	GPIO.output(10, True)
+	
+	# Red and amber for UK only, sleep longer for US
+	if (pattern == 'uk'):
+		GPIO.output(10, True)
+
 	time.sleep(1)
+
 	# Green
 	GPIO.output(9, False)
 	GPIO.output(10, False)
 	GPIO.output(11, True)
 	time.sleep(5)
-	# Amber
+	
+	# Amber, longer in US than UK
 	GPIO.output(11, False)
 	GPIO.output(10, True)
-	time.sleep(2)
+	if (pattern == 'uk'):
+		time.sleep(2)
+	else:
+		time.sleep(3)
+	
 	# Amber off (red comes on at top of loop)
 	GPIO.output(10, False)
